9a9bf66f46dd096f0c12bd30fb207a9c22561755
Khanh Lee <lee.cois.dev@gmail.com>
2025-04-25 02:37:04 +0700

    chore: api

--- src/lib/ablyClient.ts
+++ src/lib/ablyClient.ts
@@ -1,6 +1,6 @@
 import { Ably } from '@refinedev/ably';
 
-const apiKey = process.env.NEXT_PUBLIC_ABLY_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_ABLY_API_KEY || 'ably_api_key';
 if (!apiKey) {
   throw new Error('ABLY_API_KEY is not defined');
 }
